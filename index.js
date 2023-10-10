00043c8fc47632b75a757f1dc018ad4b073b98c5
Patrick Ngatia <patrickngatia14@gmail.com>
2023-10-10 19:48:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const elementRemove= document.getElementById('main','#main');
+elementRemove.remove();
+
+
+const newHeader= document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+
+newHeader.textContent = "Patrick is the champion";
+body.append(newHeader);
